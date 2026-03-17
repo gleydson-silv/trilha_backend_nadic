3f171b811964776581a68e39739c7d287a9bb8f0
Gleydson Luidy <gleydsonluidy2@gmail.com>
2026-03-17 17:12:35 -0300

    Adiciona app users

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usrs',
+    'users',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
