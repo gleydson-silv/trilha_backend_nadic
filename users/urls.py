abe98858273ad0e0306aaa58a1531e637bc1e37f
Gleydson Luidy <gleydsonluidy2@gmail.com>
2026-03-19 14:47:06 -0300

    Adiciona url de profile

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path('profile/complete/', views.complete_profile, name='complete_profile'),
 ]
