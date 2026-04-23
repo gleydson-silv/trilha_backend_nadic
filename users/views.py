85d6a48c6881d7e6a483fdd550e8735059066cb6
Gleydson Luidy <gleydsonluidy2@gmail.com>
2026-04-23 15:15:05 -0300

    Refatora view de paginção de categoria

--- users/views.py
+++ users/views.py
@@ -645,6 +645,70 @@ class CategoryPagination(PageNumberPagination):
     max_page_size = 100
 
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by("name")
+    serializer_class = CategorySerializer
+    permission_classes = [CategoryAccessPermission]
+    pagination_class = CategoryPagination
+
+    def get_paginated_response(self, data):
+        return ok_response(data=super().get_paginated_response(data).data)
+
+    @method_decorator(ratelimit(key="user", rate="60/m", method="GET"))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @method_decorator(ratelimit(key="user", rate="10/m", method="POST"))
+    def create(self, request, *args, **kwargs):
+        if getattr(request, "limited", False):
+            return error_response(
+                "Limite máximo de requisições atingido.",
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            )
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return ok_response(data=serializer.data, status_code=status.HTTP_201_CREATED)
+        return error_response("Dados inválidos.", details=normalize_serializer_errors(serializer.errors))
+
+    @method_decorator(ratelimit(key="user", rate="60/m", method="GET"))
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return ok_response(data=serializer.data)
+
+    @method_decorator(ratelimit(key="user", rate="10/m", method="PUT"))
+    def update(self, request, *args, **kwargs):
+        if getattr(request, "limited", False):
+            return error_response(
+                "Limite máximo de requisições atingido.",
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            )
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return ok_response(data=serializer.data)
+        return error_response("Dados inválidos.", details=normalize_serializer_errors(serializer.errors))
+
+    @method_decorator(ratelimit(key="user", rate="10/m", method="PATCH"))
+    def partial_update(self, request, *args, **kwargs):
+        kwargs["partial"] = True
+        return self.update(request, *args, **kwargs)
+
+    @method_decorator(ratelimit(key="user", rate="10/m", method="DELETE"))
+    def destroy(self, request, *args, **kwargs):
+        if getattr(request, "limited", False):
+            return error_response(
+                "Limite máximo de requisições atingido.",
+                status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+            )
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return ok_response(status_code=status.HTTP_204_NO_CONTENT)
+
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -828,64 +892,3 @@ def checkout(request):
         return error_response("Erro interno ao processar o pedido.", details=str(e))
 
 
-@api_view(["GET", "POST"])
-@permission_classes([CategoryAccessPermission])
-@ratelimit(key="user", rate="60/m", method="GET")
-@ratelimit(key="user", rate="10/m", method="POST")
-def categories_list_create(request):
-    if getattr(request, "limited", False):
-        return error_response(
-            "Limite máximo de requisições atingido.",
-            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-        )
-
-    if request.method == "GET":
-        queryset = Category.objects.all().order_by("name")
-        paginator = CategoryPagination()
-        page = paginator.paginate_queryset(queryset, request)
-        serializer = CategorySerializer(page, many=True)
-        paginated = paginator.get_paginated_response(serializer.data).data
-        return ok_response(data=paginated)
-
-    serializer = CategorySerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return ok_response(data=serializer.data, status_code=status.HTTP_201_CREATED)
-    return error_response("Dados inválidos.", details=normalize_serializer_errors(serializer.errors))
-
-
-@api_view(["GET", "PUT", "PATCH", "DELETE"])
-@permission_classes([CategoryAccessPermission])
-@ratelimit(key="user", rate="60/m", method="GET")
-@ratelimit(key="user", rate="10/m", method="PUT")
-@ratelimit(key="user", rate="10/m", method="PATCH")
-@ratelimit(key="user", rate="10/m", method="DELETE")
-def category_detail_update_delete(request, category_id):
-    if getattr(request, "limited", False):
-        return error_response(
-            "Limite máximo de requisições atingido.",
-            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-        )
-
-    try:
-        category = Category.objects.get(pk=category_id)
-    except Category.DoesNotExist:
-        return error_response("Categoria não encontrada.", status_code=status.HTTP_404_NOT_FOUND)
-
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
-        return ok_response(data=serializer.data)
-
-    if request.method == "DELETE":
-        category.delete()
-        return ok_response(status_code=status.HTTP_204_NO_CONTENT)
-
-    serializer = CategorySerializer(
-        category,
-        data=request.data,
-        partial=(request.method == "PATCH"),
-    )
-    if serializer.is_valid():
-        serializer.save()
-        return ok_response(data=serializer.data)
-    return error_response("Dados inválidos.", details=normalize_serializer_errors(serializer.errors))
