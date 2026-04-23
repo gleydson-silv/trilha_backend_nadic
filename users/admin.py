47ac3019e5777c57091358869414d1a7101edfe7
Gleydson Luidy <gleydsonluidy2@gmail.com>
2026-04-23 13:49:46 -0300

    Adiciona models ao painel admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity_in_stock', 'created_at']
+
+admin.site.register(Product, AdminProduct)
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', 'description', 'created_at']
+
+admin.site.register(Category, AdminCategory)
+
+
