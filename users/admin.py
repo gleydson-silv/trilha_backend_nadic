3820f3d6a1503bfeb1299626e16d5f09168a9035
Gleydson Luidy <gleydsonluidy2@gmail.com>
2026-04-23 15:08:37 -0300

    adiciona model user ao painel de admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, User
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ['name', 'price', 'quantity_in_stock', 'created_at']
@@ -11,5 +11,14 @@ class AdminCategory(admin.ModelAdmin):
     list_display = ['name', 'description', 'created_at']
 
 admin.site.register(Category, AdminCategory)
+
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'role', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['role', 'is_active', 'is_staff', 'is_superuser']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-created_at']
+    
+admin.site.register(User, AdminUser)
 
 
